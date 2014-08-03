4a1d29ec79dd9621e9ef449feb88a280d9ec5a8a
Spawek <spawnerer@gmail.com>
2014-08-03 18:08:48 +0200

    Added real image size to test

--- source/SteeringCarFromAbove/SteeringCarFromAboveTests/MapBuilderTests.cs
+++ source/SteeringCarFromAbove/SteeringCarFromAboveTests/MapBuilderTests.cs
@@ -56,16 +56,21 @@ namespace SteeringCarFromAboveTests
             MarkerFinder markerFinderMock = mocksReposiory.StrictMock<MarkerFinder>();
             ObstaclesFinder obstaclesFinderMock = mocksReposiory.StrictMock<ObstaclesFinder>();
 
+            const int IMAGE_SIZE_X = 1000;
+            const int HALF_IMAGE_SIZE_X = IMAGE_SIZE_X / 2;
+            const int IMAGE_SIZE_Y = 800;
+            const int HALF_IMAGE_SIZE_Y = IMAGE_SIZE_Y / 2;
+
             Image image = new Image(0, 0);
 
             Marker carMarker = new Marker();
             Marker parkingMarker = new Marker();
             List<Marker> stableMarkers = new List<Marker>() { new Marker() };
 
-            PositionAndOrientation carPosition = new PositionAndOrientation(5.0f, 4.0f, 0.0f);
-            PositionAndOrientation parkingPosition = new PositionAndOrientation(1.0f, 1.0f, 1.0f);
+            PositionAndOrientation carPosition = new PositionAndOrientation(HALF_IMAGE_SIZE_X + 5.0f, HALF_IMAGE_SIZE_Y + 4.0f, 0.0f);
+            PositionAndOrientation parkingPosition = new PositionAndOrientation(HALF_IMAGE_SIZE_X + 1.0f, HALF_IMAGE_SIZE_Y + 1.0f, 1.0f);
             List<PositionAndOrientation> stableMarkersPosition =
-                new List<PositionAndOrientation>() { new PositionAndOrientation(3.0f, 5.0f, 90.0f) };
+                new List<PositionAndOrientation>() { new PositionAndOrientation(HALF_IMAGE_SIZE_X + 3.0f, HALF_IMAGE_SIZE_Y + 5.0f, 90.0f) };
 
             ObjectsToTrace objectsToTrace = new ObjectsToTrace(stableMarkers, carMarker, parkingMarker);
             MapBuilder mapBuilder = new MapBuilder(markerFinderMock, obstaclesFinderMock, objectsToTrace);
@@ -85,9 +90,9 @@ namespace SteeringCarFromAboveTests
                 map = mapBuilder.BuildMap(image);
             }
 
-            PositionAndOrientation newCarPosition = new PositionAndOrientation(-4.0f, 5.0f, 90.0f);
+            PositionAndOrientation newCarPosition = new PositionAndOrientation(HALF_IMAGE_SIZE_X - 4.0f, HALF_IMAGE_SIZE_Y + 5.0f, 90.0f);
             List<PositionAndOrientation> newStableMarkersPosition =
-                new List<PositionAndOrientation>() { new PositionAndOrientation(-3.0f, 1.0f, 180.0f) };
+                new List<PositionAndOrientation>() { new PositionAndOrientation(HALF_IMAGE_SIZE_X - 3.0f, HALF_IMAGE_SIZE_Y + 1.0f, 180.0f) };
 
             Image newImage = new Image(0, 0);
             using (mocksReposiory.Record())
@@ -102,8 +107,8 @@ namespace SteeringCarFromAboveTests
                 mapBuilder.UpdateCarPosition(map, newImage);
 
                 double delta = 0.0001f;
-                Assert.AreEqual(7.0f, map.car.x, delta);
-                Assert.AreEqual(6.0f, map.car.y, delta);
+                Assert.AreEqual(HALF_IMAGE_SIZE_X + 7.0f, map.car.x, delta);
+                Assert.AreEqual(HALF_IMAGE_SIZE_Y + 6.0f, map.car.y, delta);
                 Assert.AreEqual(0.0f, map.car.angle, delta);
             }
         }
